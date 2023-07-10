33e0a469cc03190af8cadd8f1de6c4d6a90d4e1b
berenicefaure <91095063+berenicefaure@users.noreply.github.com>
2023-07-09 22:06:55 -0300

    Add files via upload
    Inicio

---
+++ index.js
@@ -0,0 +1,78 @@
+//Calcular promedio de notas de alumnos
+
+//Declaracion e iniciacion var
+let option;
+let nombreAlumno;
+let nota1;
+let nota2;
+let promedio;
+let inasistencias;
+
+
+//calculo promedio
+function promediar() {
+
+    nota1 = parseFloat(prompt("Ingresar nota primer periodo: "));
+    nota2 = parseFloat(prompt("Ingresar nota segundo periodo: "));
+    promedio = (nota1 + nota2) / 2;
+
+    alert(nombreAlumno + " recibe una nota final de: " + promedio);
+
+}
+
+//condicion libre o regular 
+function faltas() {
+
+    let inasistencias = parseFloat(prompt("Ingresar cantidad de faltas: "));
+
+    if (inasistencias > 15) {
+        alert(nombreAlumno+ " : Condición libre");
+    } else {
+        alert(nombreAlumno+ " : Condición regular");
+    }
+}
+
+
+//menu con switch
+
+do {
+    option = parseInt(prompt(
+        "REGISTRO DE ALUMNOS\n" +
+        "1. Ingresar nombre de alumno\n" +
+        "2. Calcular nota final\n" +
+        "3. Condicion final\n" +
+        "4. Salir\n" +
+        "Ingrese la opción deseada:"
+    ));
+
+    switch (option) {
+        case 1: nombreAlumno = prompt("Ingresar nombre del alumno");
+            break;
+        case 2: 
+        
+        promediar();
+
+        if (promedio > 6) {
+            alert("Felicitaciones, estás aprobado");
+        }
+        else {
+            alert("Estás desaprobado");
+        }
+
+        
+        break;
+
+        case 3:
+            faltas();
+            break;
+
+        default:
+            break;
+    }
+
+} while (option !== 4);
+
+
+
+
+
